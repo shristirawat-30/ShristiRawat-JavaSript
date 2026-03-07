b1b9cd195d75f1b0b12e30b460463c76eba6252c
shristirawat-30 <rawatshristi444@gmail.com>
2026-03-07 16:34:50 +0000

    new file

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 12345
+
+let accountEmail = "shitu@gmail.com"
+var accountpass = "12"
+
+accountCity = "dehradun"
+let accountState;  // undefined 
+//  accountId=2   // not allowed 
+accountEmail = "qwery@123"
+accountpass="23"
+accountCity="jaipur"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountpass, accountCity,accountState]);
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict";  // treat all JS code as newer version
+
+// alert(3+ 3) we are using node.js , not browser
+
+let name = "Shristi"; // string data type
+let age = 18;  //number
+let isLog = false; // boolean
+
+
+//number  => 2^53
+//bigint 
+// string =""
+// boolean => true/false
+// null -> standalone value representaion that means it is empty
+// undefined => when we have variable declared but not given value 
+// symbol -->(used in reacts for uniqueness) 
+
+// they all are primitive data types
+
+//object
+// type of null is object
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof isLog);
+console.log(typeof undefined);
+console.log(typeof null);
+
+
